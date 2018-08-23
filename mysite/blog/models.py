04ae9260363a06b19327ad2371778cc7198f640f
Kirill Denisov <kez103@mail.ru>
2018-08-23 21:32:10 +0400

    PostAdmin

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib import admin
 
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
     timestamp = models.DateTimeField()
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp')
+
+
+admin.site.register(Post, PostAdmin)
